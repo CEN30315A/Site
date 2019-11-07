1cd313bcb4edb0e2d67de8b94caa02b4c8e53af3
Sean Stanley <spstanley@ufl.edu>
2019-11-07 17:37:58 -0500

    Added NavBar functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar";
 import Section from './components/SectionComponent';
 import FaqSection from './components/FaqSection';
 
@@ -14,10 +15,21 @@ const questAns = [
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Section title="About Us" body= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend diam. Morbi finibus commodo sem vitae cursus. Sed ac justo risus. Phasellus quis dui non tortor aliquet hendrerit. Nam eu ligula ante. Maecenas fermentum lobortis ex, vitae rutrum turpis sollicitudin eget. Duis non eleifend ex, eu mollis urna. Nullam aliquet pellentesque condimentum. Integer at est ut risus eleifend fermentum a id lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean venenatis eleifend felis, id tincidunt nisl. Sed elementum, purus et accumsan imperdiet, lectus sem feugiat urna, vel ultrices risus turpis rhoncus enim."/>
-        <Section title="FAQ" body= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend diam. Morbi finibus commodo sem vitae cursus. Sed ac justo risus. Phasellus quis dui non tortor aliquet hendrerit. Nam eu ligula ante. Maecenas fermentum lobortis ex, vitae rutrum turpis sollicitudin eget. Duis non eleifend ex, eu mollis urna. Nullam aliquet pellentesque condimentum. Integer at est ut risus eleifend fermentum a id lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean venenatis eleifend felis, id tincidunt nisl. Sed elementum, purus et accumsan imperdiet, lectus sem feugiat urna, vel ultrices risus turpis rhoncus enim."/>
+        <Section 
+          title = "About Us" 
+          body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend diam. Morbi finibus commodo sem vitae cursus. Sed ac justo risus. Phasellus quis dui non tortor aliquet hendrerit. Nam eu ligula ante. Maecenas fermentum lobortis ex, vitae rutrum turpis sollicitudin eget. Duis non eleifend ex, eu mollis urna. Nullam aliquet pellentesque condimentum. Integer at est ut risus eleifend fermentum a id lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean venenatis eleifend felis, id tincidunt nisl. Sed elementum, purus et accumsan imperdiet, lectus sem feugiat urna, vel ultrices risus turpis rhoncus enim."
+          dark = {true}
+          id = "section1"
+        />
+        <Section 
+          title ="FAQ" 
+          body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut eleifend diam. Morbi finibus commodo sem vitae cursus. Sed ac justo risus. Phasellus quis dui non tortor aliquet hendrerit. Nam eu ligula ante. Maecenas fermentum lobortis ex, vitae rutrum turpis sollicitudin eget. Duis non eleifend ex, eu mollis urna. Nullam aliquet pellentesque condimentum. Integer at est ut risus eleifend fermentum a id lectus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean venenatis eleifend felis, id tincidunt nisl. Sed elementum, purus et accumsan imperdiet, lectus sem feugiat urna, vel ultrices risus turpis rhoncus enim."
+          dark = {true}
+          id = "section2"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
