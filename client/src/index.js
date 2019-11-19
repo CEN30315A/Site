50cf8a2b0a5916b40732e2c8e3f43c3e5be9110b
maysong16 <maysong@ufl.edu>
2019-11-19 16:35:03 -0500

    fixes to home picture carousel

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import data from './data/data';
-import Email from './config';
+//import Email from './config';
 
 ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 
@@ -13,31 +13,3 @@ ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: Email.email,
-    pass: Email.password
-  }
-});
-
-var mailOptions = {
-  from: Email.email,
-  to: 'nicholas.signori@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-function send() {
-  transporter.sendMail(mailOptions, function(error, info){
-  console.log("triggered");
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-})};
