6ed1778e9c8941028def4278f7d40ae24413b4a4
lawrencehua1 <lawrence.hua@ufl.edu>
2019-11-21 14:41:18 -0500

    StripeCode

---
+++ client/src/stripe/Script.js
@@ -0,0 +1,4 @@
+// Set your publishable key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = Stripe('pk_test_LOEe7VBA0Zl8LYOL4QXw3U3Y00cpT2mN0C');
+var elements = stripe.elements();
