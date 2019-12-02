180bd4f79c4a15f3e07271aade4188dc03a7f6ec
Chris Posada <c.posada@ufl.edu>
2019-12-01 20:28:36 -0500

    Order Email Confirmation
    yeet

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const keys = require("../../config/keys");
 const passport = require("passport");
+const axios = require("axios");
 
 const Order = require("../../models/Orders");
 
@@ -23,8 +24,23 @@ const Order = require("../../models/Orders");
  
 
     let order= new Order(req.body)
-    order.save()
+    order.save();
+
+    const date = new Date();
+    const day = (date.getDate() < 10 ? '0' : '') + date.getDate();
+    const month = ((date.getMonth() + 1) < 10 ?  '0' : '') + (date.getMonth() + 1); //January is 0!
+    const year = date.getFullYear();
+    var today = month + '/' + day + '/' + year;
+    axios.post('/email', {
+      "recipients": [order.email],
+      "subject": "XDG Site Order Placed",
+      "html": "<h2>Order placed with XDG site</h2> <p>You ordered " + order.quantity + " clamps on " + today + " at " + date.getHours() + ":" + date.getMinutes() + ".</p>" + "<h2>Contact Info</h2> <p>Dr. J.C. Roig </p> <p>XDG Technologies, LLC</p> <p>6485 S.W. 51 Court</p> <p>Ocala, FL. 34474-5768</p> <p>Phone: (352) 812-1175</p>"
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
     res.redirect('/');
+    
 
     //for testing the posts
     //console.log(order)
