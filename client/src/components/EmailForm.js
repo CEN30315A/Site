3f90ea3f892dad05cd43232dd7acd2404dfa76d7
Nicholas Signori <nsignori@ufl.edu>
2019-12-02 18:10:00 -0500

    email start

---
+++ client/src/components/EmailForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import * as ReactBootstrap from 'react-bootstrap';
+
+export default function EmailForm({ title, dark, id }) {
+  return (
+    <div className={"section" + (dark ? " section-dark" : "")}>
+      <div className="section-content" id={id}>
+        <h1>{title}</h1>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Pass word" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
+}
