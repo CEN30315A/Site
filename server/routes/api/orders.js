60dc61c8fda6cbd736a6479065a6fb8189b1de2d
Nicholas Signori <nsignori@ufl.edu>
2019-12-04 11:23:15 -0500

    first attempt

--- server/routes/api/orders.js
+++ server/routes/api/orders.js
@@ -15,7 +15,7 @@ const Order = require("../../models/Orders");
     const year = date.getFullYear();
     var today = month + '/' + day + '/' + year;
     axios.post('/email', {
-      "recipients": [order.email, process.env.OWNER_EMAIL],
+      "recipients": [order.email, "OWNER"],
       "subject": "XDG Site Order Placed",
       "html": "<h2>Order placed with XDG site by " + order.firstname + " " + order.lastname + "</h2> <p>You ordered " + order.quantity + " clamps on " + today + " at " + date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ".</p>" + "<h2>Contact Info</h2> <p>Dr. J.C. Roig </p> <p>XDG Technologies, LLC</p> <p>6485 S.W. 51 Court</p> <p>Ocala, FL. 34474-5768</p> <p>Phone: (352) 812-1175</p>"
     })
