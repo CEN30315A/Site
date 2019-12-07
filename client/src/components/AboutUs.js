2476485849b92fc6892e41aaa23145ad1f6a4c99
lawrencehua1 <lawrence.hua@ufl.edu>
2019-12-07 13:42:29 -0500

    added back information

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -1,12 +1,13 @@
 import React from "react";
 
+//section for the about us
+// includes title and information
 export default function AboutUs({ title, dark, id }) {
-  //Section for the about us
-  //includes title and allows for certain
   return (
     <div className={"section" + (dark ? " section-dark" : "")}>
       <div className="section-content" id={id}>
         <h1>{title}</h1>
+        <p>Juan Carlos Roig, M.D. is the inventor, co-founder, and chairman of XDG Technologies. Dr. Roig has more than 25 years of experience, and is board certified in pediatrics as well as in neonatal and perinatal medicine. He is an Assistant Professor at the University of Florida College of Medicine, Department of Pediatrics (Neonatal Division), and the Medical Director for the Neonatal Intensive Care Unit at Munroe Regional Medical Center in Ocala, FL. He also provides services at other hospitals, including the UF Health Shands Hospital in Gainesville, FL. Dr. Roig is a Fellow with the American Academy of Pediatrics and has extensive experience in managing pediatric intensive care units in public and private hospitals.</p>
       </div>
     </div>
   );
--- client/src/components/ContactUs.js
+++ client/src/components/ContactUs.js
@@ -1,17 +1,23 @@
 import React from "react";
 import EmailForm from "./EmailForm";
-
-//contact us Section including the contact form 
+//contact us section including the a email form that can send a message to the owner
 export default function ContactUs({ title, dark, id }) {
   return (
-    
     <div className={"section" + (dark ? " section-dark" : "")}>
       <div className="section-content" id={id}>
         <h1>{title}</h1>
+        <p>
+			        Dr. J.C. Roig <br/>
+			XDG Technologies, LLC<br/>
+			6485 S.W. 51 Court<br/>
+			Ocala, FL. 34474-5768<br/>
+<br/>
+			Phone: (352) 812-1175
+
+</p>
 
 <EmailForm></EmailForm>
       </div>
-    
     </div>
   );
 }
